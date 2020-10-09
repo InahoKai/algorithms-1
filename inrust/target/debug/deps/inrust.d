6cbd4eb691272b713f0ef223c376f4cac531a098
Will Lane <williamlane923@gmail.com>
2020-10-08 20:20:24 -0700

    .

---
+++ inrust/target/debug/deps/inrust.d
@@ -0,0 +1,6 @@
+/Users/willlane/dev/algoritms/inrust/target/debug/deps/inrust: src/main.rs src/reverse_words.rs
+
+/Users/willlane/dev/algoritms/inrust/target/debug/deps/inrust.d: src/main.rs src/reverse_words.rs
+
+src/main.rs:
+src/reverse_words.rs:
---
+++ inrust/target/debug/inrust.d
@@ -0,0 +1,1 @@
+/Users/willlane/dev/algoritms/inrust/target/debug/inrust: /Users/willlane/dev/algoritms/inrust/src/main.rs /Users/willlane/dev/algoritms/inrust/src/reverse_words.rs
